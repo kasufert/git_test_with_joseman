24f3f18f703ab10c94907eb9467f50d38bb899e5
kasufert <jacksimunic2017@gmail.com>
2023-01-03 22:35:36 -0800

    Added extras

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include "funk.cpp"
+#include "extras/getNum.cpp"
 
 int main()
 {
     funk();
     printf("Hello Jack\n");
     printf("Hello All\n");
+    printf("The number is: %i\n", getNum());
     return 0;
 }
