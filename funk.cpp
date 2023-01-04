6490ce2d60d4c4dc0f9dfa4fb22baac563303b39
Jose Aguillon <Jose4nrg@gmail.com>
2023-01-03 22:19:49 -0800

    added funkiness

---
+++ funk.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void funk()
+{
+    printf("called from funk\n");
+}
--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include "funk.cpp"
 
 int main()
 {
+    funk();
     printf("Hello Jack\n");
     printf("Hello All\n");
     return 0;
